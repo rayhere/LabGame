790772f06dae94f319e28530666b7f74dc43b963
Hong Lam To <raymond0586@msn.com>
2023-10-28 19:59:21 -0400

    create scenes (mainMenu, level1), scripts

---
+++ Assets/PauseResume.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseResume : MonoBehaviour
+{
+    [SerializeField] GameObject[] pauseMode;
+    [SerializeField] GameObject[] resumeMode;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseMode = GameObject.FindGameObjectsWithTag("ShowInPauseMode");
+        resumeMode = GameObject.FindGameObjectsWithTag("ShowInResumeMode");
+        ResumeMode();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0.0f;
+        PauseMode();
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1.0f;
+        ResumeMode();
+    }
+
+    public void PauseMode()
+    {
+
+        foreach (GameObject go in pauseMode)
+            go.SetActive(true);
+
+        foreach (GameObject go in resumeMode)
+            go.SetActive(false);
+    }
+
+    public void ResumeMode()
+    {
+
+        foreach (GameObject go in pauseMode)
+            go.SetActive(false);
+
+        foreach (GameObject go in resumeMode)
+            go.SetActive(true);
+    }
+}
---
+++ Assets/SettingsFunctions.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsFunctions : MonoBehaviour
+{
+    [SerializeField] GameObject[] settingMenu;
+    [SerializeField] GameObject[] mainMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        settingMenu = GameObject.FindGameObjectsWithTag("ShowInSettingMenu");
+        mainMenu = GameObject.FindGameObjectsWithTag("ShowInMainMenu");
+
+        MainMenuMode();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GoToMainMenu()
+    {
+        Time.timeScale = 1.0f;
+        MainMenuMode();
+    }
+
+    public void GoToSettingMenu()
+    {
+        Time.timeScale = 0.0f;
+        SettingMenuMode();
+    }
+
+    void SettingMenuMode()
+    {
+        foreach (GameObject go in settingMenu)
+            go.SetActive(true);
+
+        foreach (GameObject go in mainMenu)
+            go.SetActive(false);
+    }
+
+    void MainMenuMode()
+    {
+        foreach (GameObject go in settingMenu)
+            go.SetActive(false);
+
+        foreach (GameObject go in mainMenu)
+            go.SetActive(true);
+    }
+}
